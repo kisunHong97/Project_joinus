084417a97caf48531a8bfee2c084c04263591680
ahnjaeyoung10074 <young1007450@gmail.com>
2023-05-25 09:29:42 +0900

    no message

--- Joinus/src/main/java/com/ezen/joinus/controller/HomeController.java
+++ Joinus/src/main/java/com/ezen/joinus/controller/HomeController.java
@@ -94,32 +94,34 @@ public class HomeController {
     // 게시물 이동
 
     @RequestMapping(value = "/board/read", method = RequestMethod.GET)
-    public String read(@ModelAttribute("ProductVO") ProductVO productVO, Model model, @RequestParam("pno") int pno, HttpSession session){
+    public String read(@ModelAttribute("ProductVO") ProductVO productVO, Model model, @RequestParam("pno") int pno, HttpSession session) {
         // 상품 정보 가져오기(썸네일, 상세정보 포함)
-        System.out.println("pno가져오니???"+pno);
+        System.out.println("pno가져오니???" + pno);
         productVO = productService.getProductContents(pno);
         productVO.setThumbnailList(fileService.selectThumbnailList(productVO.getPno()));
         productVO.setDetail(fileService.selectDetail(productVO.getPno()));
         System.out.println(productVO);
         model.addAttribute("productVO", productVO);
         Integer sno = productVO.getSno();
-        System.out.println("sno!!!!!!!!!!"+sno);
+        System.out.println("sno!!!!!!!!!!" + sno);
         model.addAttribute("store", storeService.getStore(sno));
 
+        BusinessUserVO businessUser = (BusinessUserVO) session.getAttribute("BusinessUserVO");
+        CustomerUserVO customerloginUser = (CustomerUserVO) session.getAttribute("customerUserVO");
 
 //        BusinessUserVO businessUser = (BusinessUserVO) session.getAttribute("BusinessUserVO");
 //        model.addAttribute("store",storeService.getStore(businessUser.getBno()));
 
         // 사용자 정보 가져오기
         String u_id = (String) session.getAttribute("id");
-        System.out.println("로그인 된 사용자 아이디 불러오나?:"+u_id);
+        System.out.println("로그인 된 사용자 아이디 불러오나?:" + u_id);
         CustomerUserVO customerUserVO = customerService.getCustomerById(u_id);
         System.out.println(customerUserVO);
         model.addAttribute("customerUserVO", customerUserVO);
 
-        try{
+        try {
             WishlistVO wishlist = wishlistService.getWishlistByPnoAndUid(pno, u_id);
-            System.out.println("wishlist : " + wishlist );
+            System.out.println("wishlist : " + wishlist);
             if (wishlist != null) {
                 model.addAttribute("like", 1);
             } else {
@@ -133,14 +135,14 @@ public class HomeController {
             } else {
                 model.addAttribute("cart", 0);
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("로그인 안해서 여기 진입 합니다.");
             productService.getProductContents(pno);
         }
         System.out.println("프로덕트네임!!:" + productVO.getP_name());
         String P_name = productVO.getP_name();
         List<ReviewVO> list2 = customerService.getreview(pno);
-        System.out.println("왜 ??"+list2);
+        System.out.println("왜 ??" + list2);
         float totalRating = 0;
         float avg = 0;
         for (ReviewVO review : list2) {
@@ -153,24 +155,36 @@ public class HomeController {
 
         System.out.println("평균: " + formattedAvg);
 
-        if (u_id==null){
-            model.addAttribute("productlist",list2);
-            model.addAttribute("avg",formattedAvg);
+        if (customerloginUser == null && businessUser == null) {
+            model.addAttribute("productlist", list2);
+            model.addAttribute("avg", formattedAvg);
             model.addAttribute("listlength", list2.size());
-           // model.addAttribute("customerid",customerUserVO.getU_id());
+//            model.addAttribute("inquirylist", inquiryList);
             return "/board/read";
-        }else {
+        } else if (u_id != null && businessUser == null) {
             List<PurchaseVO> list = purchaseService.getPurchaseInfo(u_id);
-            model.addAttribute("list",list);
-            model.addAttribute("productlist",list2);
-            model.addAttribute("avg",formattedAvg);
+            model.addAttribute("list", list);
+            model.addAttribute("productlist", list2);
+            model.addAttribute("avg", formattedAvg);
             model.addAttribute("listlength", list2.size());
-            model.addAttribute("customerid",customerUserVO.getU_id());
-            System.out.println("구매한 목록list!!!!!!!!!!!"+list);
+            model.addAttribute("customerid", customerUserVO.getU_id());
+//            model.addAttribute("inquirylist", inquiryList);
+            System.out.println("구매한 목록list!!!!!!!!!!!" + list);
+            return "/board/read";
+        } else if (businessUser != null && businessUser.getB_id() != null) {
+            model.addAttribute("productlist", list2);
+            model.addAttribute("avg", formattedAvg);
+            model.addAttribute("listlength", list2.size());
+//            model.addAttribute("inquirylist", inquiryList);
+            model.addAttribute("businessUser", businessUser);
+            StoreVO getstoreVO = storeService.getStore(businessUser.getBno());
+//            List<ProductVO> productVOList = productService.selectProductListBySno(getstoreVO.getSno());
+//            model.addAttribute("productVOList",productVOList);
+            return "/board/read";
+        } else {
             return "/board/read";
         }
     }
-
     // 해당 상품을 찜 목록에 추가하는 기능
     @PostMapping("/wishlist/add")
     public ResponseEntity<String> addWishlist(WishlistVO vo, HttpSession session) {
