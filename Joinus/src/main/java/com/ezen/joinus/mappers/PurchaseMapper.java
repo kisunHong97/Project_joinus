87092689e59c1d712127a058745126459da79c8b
ahnjaeyoung10074 <young1007450@gmail.com>
2023-05-31 13:16:43 +0900

    코드 정리

--- Joinus/src/main/java/com/ezen/joinus/mappers/PurchaseMapper.java
+++ Joinus/src/main/java/com/ezen/joinus/mappers/PurchaseMapper.java
@@ -4,19 +4,22 @@ import com.ezen.joinus.vo.CustomerUserVO;
 import com.ezen.joinus.vo.ProductVO;
 import com.ezen.joinus.vo.PurchaseVO;
 import com.ezen.joinus.vo.RefundVO;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Mapper
 public interface PurchaseMapper {
+    void deleteProduct(int pno);
     void insertProduct(PurchaseVO purchaseVO);
     void updateUserPoint(CustomerUserVO customerUserVO);
     List<PurchaseVO> selectUPurchaseUserId(String u_id);
     List<PurchaseVO> getPurchaseInfoSno(int sno);
     List<PurchaseVO> getPurchaseInfoPname(int sno);
     List<PurchaseVO> getAllpurchase();
-    void deleteProduct(int pno);
+
     void nowRefundPrice(Map<String, Object> parameters);
     void refundProduct(RefundVO refundVO);
     void getRefundProduct(int pno);
